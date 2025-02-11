b734afabbe9fb4806e7896414cb5a4ced903b9e0
Faiq Ahmed <faiqaws1234@gmail.com>
2025-02-11 21:10:19 +0100

    header complete, now custom logi/sign page, all ok

---
+++ app/layout.js
@@ -0,0 +1,48 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import  Header from "@/components/header"
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+const inter = Inter({subsets: ["latin"]})
+
+export const metadata = {
+  title: "AI Career Coach",
+  description: "AI Career Coach for eveyone",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider appearance={
+      {
+        baseTheme: dark,
+      }
+    } >
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${inter.className} `}
+      >
+<ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+       <Header/>
+            <main className="min-h-screen" >
+            {children}
+            </main>
+
+            {/* Footer */}
+            <footer className="bg-muted/50 py-12" >
+              <div className="container mx-auto px-4 text-center text-gray-200" >
+                <p>Made with love </p>
+              </div>
+            </footer>
+
+          </ThemeProvider>      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
